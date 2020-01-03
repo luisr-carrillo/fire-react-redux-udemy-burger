41073a57d12b6d46395773862e86ca88a5a84ecc
= <luisr_carrillo@outlook.com>
2020-01-03 16:20:31 -0600

    Cambios menores

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -64,8 +64,9 @@ export const fetchOrdersStart = () => {
 export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        const queryParams = `?auth=${token}&orderBy="userId"&equalTo="${userId}"`;
-        axios.get('/orders.json' + queryParams)
+        // const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        const queryParams =`?auth=${token}&orderBy="userId"&equalTo="${userId}"`;
+        axios.get( '/orders.json' + queryParams)
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
